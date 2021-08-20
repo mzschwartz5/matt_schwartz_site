e6e832062783e4103dd71c31b88e96af5e66b34f
Matthew Schwartz <mzschwartz5@gmail.com>
2021-08-20 14:19:15 -0400

    Added basic scrolling-based opacity to timeline

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -5,13 +5,62 @@ import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
 import TimelineConnector from '@material-ui/lab/TimelineConnector';
 import TimelineContent from '@material-ui/lab/TimelineContent';
 import TimelineDot from '@material-ui/lab/TimelineDot';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
 import { makeStyles } from "@material-ui/core/styles";
+import useIntersectionObserver from "../hooks/useIntersectionObserver";
+import { useRef } from "react";
+import { Theme } from "@material-ui/core";
 
 /*
     Main page - Container for whole site.
     In the future, will add routing to control when each tab renders and to be able to link to a specific tab.
 */
-const useStyles = makeStyles((theme) => ({
+const HomePage: React.FunctionComponent = (): JSX.Element =>
+{
+    return(
+    <>
+        <Timeline>
+            <TimelineItemWrapper/>
+            <TimelineItemWrapper/>
+            <TimelineItemWrapper/>
+        </Timeline>
+    </>
+    );
+}
+
+const TimelineItemWrapper: React.FunctionComponent = () =>
+{
+    const ref = useRef(null);
+    const thresholds = Array(10).fill(0.1).map((val,idx) => (val += 0.1*idx));
+    const entry = useIntersectionObserver(ref, {threshold: thresholds})
+    const classes = useStyles({contentOpacity: Number(entry?.intersectionRatio)});
+
+    return(
+        <TimelineItem ref={ref} classes={{missingOppositeContent: classes.missingOppositeNoStyle, root: classes.timelineItemContent}}>
+            <TimelineSeparator>
+                <TimelineDot />
+                <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent className={classes.timelineItemText}>
+                Education
+                <Card className={classes.card}>
+                    <CardMedia 
+                        image="/SlideShowImages/with_friends_at_rice.jpg"
+                        title="Rice University event with friends"
+                        className={classes.cardHeight}
+                    />
+                    <CardContent className={classes.cardContent}>
+                        Here is some placeholder text that will be underneath the picture at Rice with my friends.
+                    </CardContent>
+                </Card>
+            </TimelineContent>
+        </TimelineItem>
+    );
+}
+
+const useStyles = makeStyles<Theme, {contentOpacity: number}>({
     // Remove the blank space before the timeline line
     missingOppositeNoStyle: {
         "&:before": {
@@ -20,45 +69,28 @@ const useStyles = makeStyles((theme) => ({
     },
 
     timelineItemText: {
-        textAlign: 'left',
+        textAlign: 'left',  
     },
 
-    timelineItemContent: {
-        height: '00px',
+    timelineItemContent: ({contentOpacity}) => ({
+        height: '150vh',
+        opacity: contentOpacity,
+    }),
+
+    cardHeight: {
+        height: '125vh',
     },
 
-}));
+    cardContent: {
+        backgroundColor: 'grey',
+    },
 
-const HomePage: React.FunctionComponent = (): JSX.Element =>
-{
-    const classes = useStyles();
+    card: {
+        marginTop: '20px',
+        marginLeft: '20px',
+    },
 
-    return(
-    <>
-        <Timeline>
-            <TimelineItem classes={{missingOppositeContent: classes.missingOppositeNoStyle, root: classes.timelineItemContent}}>
-                <TimelineSeparator>
-                    <TimelineDot />
-                    <TimelineConnector />
-                </TimelineSeparator>
-                <TimelineContent className={classes.timelineItemText} >Eat</TimelineContent>
-            </TimelineItem>
-            <TimelineItem classes={{missingOppositeContent: classes.missingOppositeNoStyle, root: classes.timelineItemContent}}>
-                <TimelineSeparator>
-                    <TimelineDot />
-                    <TimelineConnector />
-                </TimelineSeparator>
-                <TimelineContent className={classes.timelineItemText}>Code</TimelineContent>
-            </TimelineItem>
-            <TimelineItem classes={{missingOppositeContent: classes.missingOppositeNoStyle, root: classes.timelineItemContent}}>
-                <TimelineSeparator>
-                    <TimelineDot />
-                </TimelineSeparator>
-                <TimelineContent className={classes.timelineItemText}>Sleep</TimelineContent>
-            </TimelineItem>
-        </Timeline>
-    </>
-    );
-}
+});
+
 
 export default HomePage;
