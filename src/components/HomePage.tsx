d4178a546c533d016b23ffecc17048c379f7c5fa
Matthew Schwartz <mzschwartz5@gmail.com>
2021-08-22 00:04:43 -0400

    Minor changes to fade-in scrolling, added header bar, changed vscode debug config files

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -8,10 +8,11 @@ import TimelineDot from '@material-ui/lab/TimelineDot';
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
+import AppBar from '@material-ui/core/AppBar';
 import { makeStyles } from "@material-ui/core/styles";
 import useIntersectionObserver from "../hooks/useIntersectionObserver";
 import { useRef } from "react";
-import { Theme } from "@material-ui/core";
+import { Slide, Theme, useScrollTrigger } from "@material-ui/core";
 
 /*
     Main page - Container for whole site.
@@ -20,7 +21,8 @@ import { Theme } from "@material-ui/core";
 const HomePage: React.FunctionComponent = (): JSX.Element =>
 {
     return(
-    <>
+    <>  
+        <HidingTitleBar/>
         <Timeline>
             <TimelineItemWrapper/>
             <TimelineItemWrapper/>
@@ -32,21 +34,24 @@ const HomePage: React.FunctionComponent = (): JSX.Element =>
 
 const TimelineItemWrapper: React.FunctionComponent = () =>
 {
+    // When an observed element passes a threshold of on-screen visibility, update its opacity accordingly for fade effect.
     const ref = useRef(null);
     const thresholds = Array(10).fill(0.1).map((val,idx) => (val += 0.1*idx));
-    const entry = useIntersectionObserver(ref, {threshold: thresholds})
-    const classes = useStyles({contentOpacity: Number(entry?.intersectionRatio)});
+    const entry = useIntersectionObserver(ref, {threshold: thresholds})             
+    const classes = useTimelineStyles({intersectionRation: Number(entry?.intersectionRatio)});
 
     return(
-        <TimelineItem ref={ref} classes={{missingOppositeContent: classes.missingOppositeNoStyle, root: classes.timelineItemContent}}>
+        <TimelineItem classes={{missingOppositeContent: classes.missingOppositeNoStyle, root: classes.timelineItemContent}}>
             <TimelineSeparator>
-                <TimelineDot />
+                <TimelineConnector />
+                <TimelineDot color="secondary" variant="outlined"/>
                 <TimelineConnector />
             </TimelineSeparator>
             <TimelineContent className={classes.timelineItemText}>
                 Education
                 <Card className={classes.card}>
-                    <CardMedia 
+                    <CardMedia
+                        ref={ref} 
                         image="/SlideShowImages/with_friends_at_rice.jpg"
                         title="Rice University event with friends"
                         className={classes.cardHeight}
@@ -60,7 +65,27 @@ const TimelineItemWrapper: React.FunctionComponent = () =>
     );
 }
 
-const useStyles = makeStyles<Theme, {contentOpacity: number}>({
+const HidingTitleBar: React.FunctionComponent = (): JSX.Element =>
+{
+    const trigger = useScrollTrigger();
+    const classes = useTitleBarStyles();
+
+    return (
+    <Slide appear={false} direction="down" in={!trigger}>
+        <AppBar className={classes.root}>
+            <SiteHeader/>
+        </AppBar>
+    </Slide>
+    );
+}
+
+const useTitleBarStyles = makeStyles({
+    root: {
+        flexDirection: 'row',
+    },
+});
+
+const useTimelineStyles = makeStyles<Theme, {intersectionRation: number}>({
     // Remove the blank space before the timeline line
     missingOppositeNoStyle: {
         "&:before": {
@@ -72,9 +97,10 @@ const useStyles = makeStyles<Theme, {contentOpacity: number}>({
         textAlign: 'left',  
     },
 
-    timelineItemContent: ({contentOpacity}) => ({
+    timelineItemContent: ({intersectionRation}) => ({
         height: '150vh',
-        opacity: contentOpacity,
+        opacity: (Math.pow(intersectionRation,2)),
+        marginLeft: '20px'
     }),
 
     cardHeight: {
