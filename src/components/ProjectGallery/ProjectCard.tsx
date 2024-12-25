c3f639971d34e330bf41df603c1a5512aaf38b2a
Matt Schwartz <mzschwartz5@gmail.com>
2024-12-25 16:12:22 -0500

    Switches to react spring for animation

--- src/components/ProjectGallery/ProjectCard.tsx
+++ src/components/ProjectGallery/ProjectCard.tsx
@@ -5,7 +5,7 @@ import { IProject } from "../../data/projects_db";
 import IconLink from "../core/IconLink";
 import GitHubIcon from '@material-ui/icons/GitHub';
 import { useMouse } from "../../hooks/useMouse";
-import { useState, useEffect } from "react";
+import { useSpring, animated } from '@react-spring/web';
 
 interface IProjectCard {
     project: IProject;
@@ -15,42 +15,44 @@ const ProjectCard: React.FunctionComponent<IProjectCard> = (props:IProjectCard):
 {
     const {project} = props;
     const [mouseState, ref] = useMouse();
-    const [cardTransitionNeeded, setCardTransitionNeeded] = useState(false);
-    const classes = useCardStyles({x: mouseState.x, y: mouseState.y, transitionNeeded: cardTransitionNeeded});
+    const classes = useCardStyles();
+
+    const springProps = useSpring({
+        transform: `rotateY(${-10 * mouseState.x}deg) rotateX(${-10 * mouseState.y}deg)`,
+        config: { tension: 300, friction: 10 },
+    });
 
     return(
         <ImageListItem sx={{lineHeight: "inherit", perspective: "1000px"}}>
-            <Card className={classes.card}
-                  ref={ref}
-                  onMouseEnter={() => setCardTransitionNeeded(true)}
-                  onMouseLeave={() => setCardTransitionNeeded(true)}
-                  onTransitionEnd={() => setCardTransitionNeeded(false)}>
-                <CardMedia
-                    className={classes.cardMedia}
-                    image={project.featuredImage ?? defaultImage}
-                    title={project.title}
-                    component="img"
-                />
-                <CardHeader
-                    title={project.title}
-                    subheader={project.dateStarted.toDate().toDateString()}
-                    className={classes.cardBackground}
-                    subheaderTypographyProps={{variant: "subtitle1"}}
-                />
-                <CardContent className={classes.cardBackground}>
-                    {project.description}
-                </CardContent>
-                <CardActions className={classes.cardActions}>
-                    <IconLink image={<GitHubIcon/>} alignRight={true} altText="GitHub link" linkTo={project.githubUrl}/>
-                </CardActions>
-            </Card>
+            <animated.div style={springProps}>
+                <Card className={classes.card} ref={ref}>
+                    <CardMedia
+                        className={classes.cardMedia}
+                        image={project.featuredImage ?? defaultImage}
+                        title={project.title}
+                        component="img"
+                    />
+                    <CardHeader
+                        title={project.title}
+                        subheader={project.dateStarted.toDate().toDateString()}
+                        className={classes.cardBackground}
+                        subheaderTypographyProps={{variant: "subtitle1"}}
+                    />
+                    <CardContent className={classes.cardBackground}>
+                        {project.description}
+                    </CardContent>
+                    <CardActions className={classes.cardActions}>
+                        <IconLink image={<GitHubIcon/>} alignRight={true} altText="GitHub link" linkTo={project.githubUrl}/>
+                    </CardActions>
+                </Card>
+            </animated.div>
         </ImageListItem>
     );
 }   
 
 export default ProjectCard;
 
-const useCardStyles = makeStyles<Theme, {x: number, y: number, transitionNeeded: boolean}>(theme => {
+const useCardStyles = makeStyles<Theme>(theme => {
     
     const paperColor = theme.palette.paper.main;
     const textColor = theme.palette.text.primary;
@@ -58,15 +60,11 @@ const useCardStyles = makeStyles<Theme, {x: number, y: number, transitionNeeded:
 
     return({
 
-        card: ({x, y, transitionNeeded}) => ({
-            transform: `translate3d(0px, 0px, 0.01px) rotateX(${y * -10}deg) rotateY(${x * -10}deg)`,
+        card: {
             borderRadius: "5px",
             margin: "10px",
             boxShadow: "10px 10px 5px 1px rgba(0,0,0,0.25)",
-            // Push card forward onto new layer to allow for hardware acceleration
-            transition: `${transitionNeeded ? "transform 0.1s ease-out" : ""}`,
-            willChange: "transform", // helps browser optimize the animation
-        }),
+        },
     
         cardMedia: {
         },
