46856a73dcbe6eeda5fd8d87fbd9f8b84cc62604
Matt Schwartz <mzschwartz5@gmail.com>
2024-12-25 22:49:49 -0500

    Stop link click propagation

--- src/components/core/IconLink.tsx
+++ src/components/core/IconLink.tsx
@@ -5,15 +5,20 @@ interface IIconLinkProps {
     linkTo: string;
     altText: string;
     alignRight?: boolean;
+    propagateClick?: boolean;
 }
 
 const IconLink: React.FunctionComponent<IIconLinkProps> = (props:IIconLinkProps): JSX.Element =>
 {
-    const {image, linkTo, altText, alignRight = false} = props;
+    const {image, linkTo, altText, alignRight = false, propagateClick = false} = props;
     const classes = useStyles({alignRight: alignRight});
 
+    const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+        if (!propagateClick) e.stopPropagation();
+    }
+
     return(
-        <a href={linkTo} target="_blank" rel="noreferrer" className={classes.icon} title={altText} >
+        <a href={linkTo} target="_blank" rel="noreferrer" className={classes.icon} title={altText} onClick={handleClick} >
             {image}
         </a>
     );
