422495ade95e859f86677418a38a82e37c360a4f
Matt Schwartz <mzschwartz5@gmail.com>
2026-03-19 00:16:28 -0400

    Remove console log

--- src/data/projects_db.tsx
+++ src/data/projects_db.tsx
@@ -42,7 +42,6 @@ export async function loadAllProjects() {
             }
 
             docs.push(project);
-            console.log(project.orderID);
         });
         docs.sort(sortProjectsByOrderID);
     }
